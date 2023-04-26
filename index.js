66b3be56fe3a2106e4401df74fa561bee715d328
shirefhamdy <shirefhamdy2338@gmail.com>
2023-04-26 08:36:04 +0200

    all files added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('uploads'));
+const cors = require("cors");
+app.use(cors());
+
+const auth = require("./routes/Auth");
+const porducts = require("./routes/products");
+
+app.listen(4000,"localhost",()=>{
+    console.log("SERVER IS RUNNING !");
+});
+
+app.use("./routes/Auth.js",auth);
+app.use("./routes/Products.js",porducts);
+
+console.log("ALL IS DONE !");

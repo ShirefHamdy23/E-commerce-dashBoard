f7561241e21f4a5a6570fa738d775dc5f1302574
shirefhamdy <shirefhamdy2338@gmail.com>
2023-05-02 22:39:55 +0300

    signing & registers

--- index.js
+++ index.js
@@ -2,24 +2,36 @@ const express = require("express");
 const app = express();
 const db = require("./DB/dbConnection");
 const userRouter = require("./routes/usersRoutes");
+const IP = require('ip')
 db.connection;
+
+app.get("/", function (req, res) {
+    console.log(req.socket.remoteAddress);
+    console.log(req.ip);
+    res.send("your IP is: " + req.ip);
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("server running on port: " + PORT);
+});
+
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-app.set('front-end','/front-end');
+app.set('front-end', '/front-end');
 const auth = require("./routes/Auth");
-const porducts = require("./routes/products");
+const products = require("./routes/products");
 const { cookie } = require("express-validator");
 
-app.listen(5500,"localhost",()=>{
+app.listen(3000,"localhost",()=>{
     console.log("SERVER IS RUNNING !");
 });
 
-app.use("./users",userRouter);
-app.use("./routes/Auth.js",auth);
-app.use("./routes/Products.js",porducts);
+app.use("./users", userRouter);
+app.use("./routes/Auth.js", auth);
+app.use("./routes/products.js", products);
 console.log("ALL IS DONE !");
 
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,25 +1,48 @@
 const express = require('express');
 const db = require('../DB/dbConnection');
 const { Result } = require('express-validator');
-const {connection} = require('../DB/dbConnection');
+const { connection } = require('../DB/dbConnection');
 const router = express.Router();
 
-router.post('/signup',async(req,res,next)=>{
+
+router.post('/signup', async (req, res, next) => {
     console.log("Hello From DB");
     res.send('hello from response');
-    let sqlCheck = ` select Email from user where Email = ${res[0]} `
-    db.query(sqlCheck,(req,res,fields)=>{
-        console.log(res[0].Email);
+    let sqlCheck = ` select email from users where email = ${req[0]} `;
+    db.query(sqlCheck, (req, res, fields) => {
+        sqlResult = res[0];
     });
-    if (sqlCheck == res[0]){
-        console.log('user already exist');
-    }else{
-        let sqlINsert = "INSERT INTO `user` (`email`, `firstName`, `lastName`, `password`,`phone`,`type`, `status`) VALUES (`${res[0]}`, `res[1]`, `res[2]`, `res[3]`, `res[4]`, 'active', 'user')"
-        db.query(sqlINsert,(req,res,fields)=>{
-            console.log('user added');
+    if (sqlResult == req[0]) {
+        res.send('user already exist');
+    } else {
+        let sqlINsert = "INSERT INTO `users` (`email`, `firstName`, `lastName`, `password`,`phone`,`type`, `status`) VALUES (`${res[0]}`, `res[1]`, `res[2]`, `res[3]`, `res[4]`, 'active', 'user')"
+        db.query(sqlINsert, (req, res, fields) => {
+            res.send('user added');
         });
     }
-    console.log(db.state());
+    console.log('it works');
+    db.end();
+})
+router.post('/signIn', async (req, res, next) => {
+    console.log("Hello From DB");
+    res.send('hello from response');
+    let sqlCheck = ` select email from users where email = ${req[0]} `;
+    db.query(sqlCheck, (req, res, fields) => {
+        sqlResult = res[0];
+    });
+    if (sqlResult == req[0]) {
+        sqlCheck = ` select password from users where password = ${req[1]} `;
+        db.query(sqlCheck, (req, res, fields) => {
+            sqlResult = res[1];
+        });
+        if(sqlResult != req[1]){
+            'incorrect password'
+        }else{
+            'logged in successfully';}
+    } else {
+        res.send('incorrect email or password');
+    }
+    console.log('it works');
     db.end();
 })
 module.exports = router;
